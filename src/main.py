949b00e623839759b7b24a78bd853f13a67ea4af
Aaditya-735 <aadityagupta735@gmail.com>
2026-06-05 17:55:52 +0530

    Implemented ATS scoring using sentence embeddings

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 from parser import extract_resume_text
+from ats_engine import calculate_ats_score
 
-resume = extract_resume_text(
+resume_text = extract_resume_text(
     "C:/Users/AG/Documents/Aaditya_Resume(Updated).pdf"
 )
 
-print(resume)
+with open(
+    "data/job_descriptions/ml_engineer.txt",
+    "r",
+    encoding="utf-8"
+) as f:
+
+    job_description = f.read()
+
+score = calculate_ats_score(
+    resume_text,
+    job_description
+)
+
+print(f"\nATS Score: {score}%")
